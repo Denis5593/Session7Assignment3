8de0f2da2cb524358ff441f75038dcea5d9c93cd
Denis5593 <39754431+Denis5593@users.noreply.github.com>
2018-08-20 22:23:48 +0530

    Add files via upload

---
+++ Session 7_Assignment 7.3.R
@@ -0,0 +1,5 @@
+# 1.	Create a box and whisker plot by class using mtcars dataset.
+
+boxplot(mpg~cyl,data=mtcars, main="Car Mileage", 
+        xlab="Number of Cylinders", ylab="Miles Per Gallon")
+
